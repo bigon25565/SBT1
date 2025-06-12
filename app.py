011240d6f63f6237aadfeb97dd80d84b73095e40
Gkamentsev <54892100+bigon25565@users.noreply.github.com>
2025-06-12 16:27:56 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,8 +3,27 @@ from flask import Flask, Response
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def root():
     response = Response("1147329")
     response.headers["X-Author"] = "1147329"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@app.route("/login/")
+def login():
+    response = Response("1147329")
+    response.headers["Content-Type"] = "text/plain; charset=UTF-8"
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    return response
+
+@app.route("/sample/")
+def sample():
+    code = """
+function task(x) {
+    return x * this * this;
+}
+""".strip()
+    response = Response(code)
+    response.headers["Content-Type"] = "text/plain; charset=UTF-8"
     response.headers["Access-Control-Allow-Origin"] = "*"
     return response
